b9729c518c6368182db3c213a69017fda99b7b0e
Sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2024-02-10 02:05:25 +0530

    inline html and scss or css

--- Angular/angular-app/src/app/app.component.ts
+++ Angular/angular-app/src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // template: '<h1>Hello World!</h1>',
+  styleUrls: ['./app.component.scss'],
+  // styles: ['h1  { color: red }'],
 })
 export class AppComponent {
   title = 'angular-app';
