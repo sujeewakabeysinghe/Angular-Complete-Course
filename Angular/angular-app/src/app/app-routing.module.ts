591daa91ea1102c1e7812da3867b0ae9569a0e45
Sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2024-02-25 01:51:22 +0530

    Router

--- Angular/angular-app/src/app/app-routing.module.ts
+++ Angular/angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PipesComponent } from './pipes/pipes.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { BindingSyntaxComponent } from './binding-syntax/binding-syntax.component';
+import { ServiceComponent } from './service/service.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/directives', pathMatch: 'full' },
+  { path: 'pipes', component: PipesComponent },
+  { path: 'directives', component: DirectivesComponent },
+  { path: '**', component: BindingSyntaxComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
