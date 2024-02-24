6f50cdf2975b982ad8fdc063637ebf81e26362fb
Sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2024-02-24 23:44:57 +0530

    async pipe

--- Angular/angular-app/src/app/pipes/pipes.component.ts
+++ Angular/angular-app/src/app/pipes/pipes.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { InterfaceFor } from '../directives/interface';
+import { Subscription } from 'rxjs';
+import { ServiceTwoService } from '../services/service-two.service';
 
 @Component({
   selector: 'gtn-pipes',
   templateUrl: './pipes.component.html',
   styleUrls: ['./pipes.component.scss']
 })
-export class PipesComponent implements OnInit {
+export class PipesComponent implements OnInit, OnDestroy {
   
   date: Date = new Date();
   case = 'suJeeWa';
@@ -20,9 +22,27 @@ export class PipesComponent implements OnInit {
   ]
   slice = 'Slice';
 
-  constructor() { }
+  subscription!: Subscription;
+  roomList: any;
+
+  rooms$ = this.serviceTwo.getRooms$;
+
+  constructor(
+    private serviceTwo: ServiceTwoService
+  ) { }
+
+  ngOnDestroy(): void {
+    // if(this.subscription) {
+    //   this.subscription.unsubscribe();
+    // }
+    // you don not need this if you are using async pipe to subscribe data stream
+  }
 
   ngOnInit(): void {
+    // this.subscription = this.serviceTwo.getRooms$.subscribe( room => {
+    //   this.roomList = room
+    // });
+    // you don not need this if you are using async pipe to subscribe data stream
   }
 
 }
