bdc037a7741eb724c30e0141acf1e5d04067eb16
Sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2024-02-25 10:03:37 +0530

    Activated Routes - 2

--- Angular/angular-app/src/app/activated-routes/activated-routes.component.ts
+++ Angular/angular-app/src/app/activated-routes/activated-routes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'gtn-activated-routes',
@@ -9,6 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ActivatedRoutesComponent implements OnInit {
 
   param!: any;
+  param$!: Observable<string>; // id is string
 
   constructor(
     private router: ActivatedRoute
@@ -17,11 +19,20 @@ export class ActivatedRoutesComponent implements OnInit {
   ngOnInit(): void {
     // this.param = this.router.snapshot.params['paramID']; // if you use this, this will not updated the same view
 
-    this.router.params.subscribe( params => {
-      console.log(params);
-      this.param = params;
-      // this.param = params['paramID'];
-    })
+    // this.router.params.subscribe( params => {
+    //   console.log(params);
+    //   this.param = params;
+    //   // this.param = params['paramID'];
+    // })
+
+    this.param$ = this.router.params.pipe(
+      map( params => params['paramID'])
+    )
+
+    // this.param$ = this.router.paramMap.pipe(
+    //   map( params => params.get('paramID'))
+    // )
+    // another way to access more
   }
 
 }
